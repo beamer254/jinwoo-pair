295e03082bc7968057f1b2ef2a7d4b19378e198a
beamer254 <jinwoobot254@gmail.com>
2025-04-10 22:07:39 +0300

    Update mega.js

--- mega.js
+++ mega.js
@@ -1,8 +1,8 @@
 const mega = require("megajs");
 
 const auth = {
-    email: 'malvinb017@gmail.com',   //use your real vaild mega account email
-    password: 'malvin266',  ////use your real vaild mega account password
+    email: 'jinwoobot254@gmail.com',   //use your real vaild mega account email
+    password: 'jinwoobot254',  ////use your real vaild mega account password
     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
 };
 
